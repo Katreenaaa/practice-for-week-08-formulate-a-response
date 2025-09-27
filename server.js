b817596350668f7c3328533a885507ac0aab0caa
Katreenaaa <katrinaemegbagha@gmail.com>
2025-09-27 10:09:01 +0100

    Completed practice for week 8 formulate a response

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 // Your code here
+const http = require("http");
+
+const responseBody = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World!</title>
+</head>
+<body>
+  <h1>Hello there!</h1>
+</body>
+</html>`;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(responseBody);
+});
+
+server.listen(5000, () => {
+  console.log("Server running at http://localhost:5000/");
+});
